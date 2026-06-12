385d008f758bfb2ba53e935fcf3a8eb50847f1b4
HassanIQ777 <hassaniq793@gmail.com>
2026-06-12 22:10:06 +0300

    some changes to install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ echo "Installing $PROG_NAME..."
 NEW_PATH="$HOME/bin/"
 SCRIPT_NAME="termux-url-opener"
 
+chmod +x install.sh termux-url-opener
+mkdir -p "$HOME/bin"
+
 echo "Copying '$SCRIPT_NAME' to '$NEW_PATH'"
 if cp "$SCRIPT_NAME" "$NEW_PATH"; then
     echo "Successfully installed $PROG_NAME!"
