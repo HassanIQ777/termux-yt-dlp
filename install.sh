5b8c83cd30f6b0d0c07f35e7a88e1a22b83afbd7
HassanIQ777 <hassaniq793@gmail.com>
2026-06-12 19:26:56 +0300

    Initial commit (code not finished)

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo " ----- Started Installer ----- "
+echo "Installing Termux YT DLP..."
+NEW_PATH="$HOME/bin/"
+SCRIPT_NAME="termux-url-opener"
+
+echo "Copying '$SCRIPT_NAME' to '$NEW_PATH'"
+cp "$SCRIPT_NAME" "$NEW_PATH"
+
