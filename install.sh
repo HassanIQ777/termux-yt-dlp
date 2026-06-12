21de6c2a5c0c769d827b42b75f654f9213593aa3
HassanIQ777 <hassaniq793@gmail.com>
2026-06-12 19:32:49 +0300

    Finished install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+PROG_NAME="Termux YT DLP"
 echo " ----- Started Installer ----- "
-echo "Installing Termux YT DLP..."
+echo "Installing $PROG_NAME..."
 NEW_PATH="$HOME/bin/"
 SCRIPT_NAME="termux-url-opener"
 
 echo "Copying '$SCRIPT_NAME' to '$NEW_PATH'"
-cp "$SCRIPT_NAME" "$NEW_PATH"
+if cp "$SCRIPT_NAME" "$NEW_PATH" -eq 0; then
+    echo "Successfully installed $PROG_NAME!"
+    echo "Note: if you make any updates or changes to '$SCRIPT_NAME', just rerun the install script!"
+else
+    echo "Failed to install $PROG_NAME"
 
+fi
